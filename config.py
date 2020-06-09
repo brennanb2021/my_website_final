50a44a7ebe5dce66c61820762457a9b46ae0355b
brennanb2021 <brennanb2021@headroyce.org>
2020-06-09 11:28:12 -0700

    testing

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+from tempfile import mkdtemp # used to create a temp directory
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'th1515453cr3tk37'
