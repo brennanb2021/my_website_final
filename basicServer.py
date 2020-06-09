751362949b6090d93ed256e222b3bf2e548fefc3
brennanb2021 <brennanb2021@headroyce.org>
2020-06-09 13:25:49 -0700

    testing

--- basicServer.py
+++ basicServer.py
@@ -1,21 +1,5 @@
 from app import app
 #from app.models import User
-from flask import Flask
-import os
-import os
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-from app import routes
-from app.CsrfDb import models
-
 
 if __name__ == '__main__': #on flask run include: -p 5372 after to run it on port 5372.
     app.run(port=33507)
