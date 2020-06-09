3f5f7062ea40ab971c241c8f96d4a947c3497992
brennanb2021 <brennanb2021@headroyce.org>
2020-06-09 14:07:08 -0700

    tried to fix redirects not working

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,5 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
-
 from app import routes
 from app.CsrfDb import models
