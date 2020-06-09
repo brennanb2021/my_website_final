0da85d4ffde02cdb352a9368348230ab5771d9a1
brennanb2021 <brennanb2021@headroyce.org>
2020-06-09 10:49:10 -0700

    testing

--- app/routes.py
+++ app/routes.py
@@ -137,7 +137,7 @@ def registerPost():
 def view():
     sessionID = request.cookies.get("user") #get the session token from the previous page cookies
     if not(checkUserLoggedIn(sessionID)):
-        console.log("on view")
+        print("on view")
         return redirect(url_for('index'))
 
     id = sessionTokens[sessionID].id #if they go to the url without inputting an id, set it to the id of the user currently logged in
@@ -287,7 +287,7 @@ def changeAccountInformation():
 def mainPage():
     sessionID = request.cookies.get("user") #get the session token from the previous page cookies
     if not(checkUserLoggedIn(sessionID)):
-        console.log("on mainpage")
+        print("on mainpage")
         return redirect(url_for('index'))
 
     id = sessionTokens[sessionID].id #get id of user logged in
@@ -308,7 +308,7 @@ News scraper
 def newsScraperGet():
     sessionID = request.cookies.get("user") #get the session token from the previous page cookies
     if not(checkUserLoggedIn(sessionID)):
-        console.log("on newsScraper")
+        print("on newsScraper")
         return redirect(url_for('index'))
     
     with open('./app/templates/scraperPage.html','w') as fCopy: #reset file
@@ -440,7 +440,7 @@ grouping app
 def pairGet():
     sessionID = request.cookies.get("user") #get the session token from the previous page cookies
     if not(checkUserLoggedIn(sessionID)):
-        console.log("on pair")
+        print("on pair")
         return redirect(url_for('index'))
 
     with open('./app/templates/pair.html','w') as fCopy: #reset file
@@ -484,7 +484,7 @@ Bullet Hell game
 def runBulletHellGame():
     sessionID = request.cookies.get("user") #get the session token from the previous page cookies
     if not(checkUserLoggedIn(sessionID)):
-        console.log("on bulleth ell")
+        print("on bulleth ell")
         return redirect(url_for('index'))
     return render_template('bulletHellGame.html')
 
