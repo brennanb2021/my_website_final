1fa48e6e453433b0759f3b90fa20276938286ecb
brennanb2021 <brennanb2021@headroyce.org>
2020-07-05 00:27:21 -0700

    testing webdriver

--- app/newsScraperDebate/mainAction.py
+++ app/newsScraperDebate/mainAction.py
@@ -8,7 +8,7 @@ def mainActionDebate(keywords, sources):
     keywords = keywords.lower()
     keywordArr = keywords.split(", ")
     options = Options()
-    
+    print("FODISFIODSFOIDSOFDSJIOIFDJS", str(os.environ.get('CHROMEDRIVER_PATH')))
     options.add_argument("--incognito")
     driver = webdriver.Chrome(str(os.environ.get('CHROMEDRIVER_PATH')), options=options)
     driver.set_page_load_timeout(5)
