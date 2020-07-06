262b77046f2e65904004752387756a26e0aab967
brennanb2021 <brennanb2021@headroyce.org>
2020-07-05 18:38:41 -0700

    request limit 30

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import os
-import os
+from flask_socketio import SocketIO, emit
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+socketio = SocketIO(app) 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
